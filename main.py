2a633c2841c87e7a8c012eb780f292731056981a
Mr.Jestrik The Jester <legend.gm3121@gmail.com>
2023-11-12 23:20:17 +0800

    Update main.py
    changed the keyboard bind to mouse bind!

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 # Made by im-razvan - CS2 TriggerBot W/O Memory Writing
-import pymem, pymem.process, keyboard, time
-from pynput.mouse import Controller, Button
-from win32gui import GetWindowText, GetForegroundWindow
+import pymem, pymem.process, time, win32api, win32con
 from random import uniform
 
-mouse = Controller()
-
 # https://github.com/a2x/cs2-dumper/
-dwEntityList = 0x17995C0
-dwLocalPlayerPawn = 0x1886C48
-m_iIDEntIndex = 0x1524
+dwEntityList = 0x17ADAE0
+dwLocalPlayerPawn = 0x16B9388
+m_iIDEntIndex = 0x153C
 m_iTeamNum = 0x3BF
 m_iHealth = 0x32C
 
-triggerKey = "shift"
+dwForceJump = 0x16B2820
+m_fFlags = 0x3C8
+
+width = win32api.GetSystemMetrics(0)
+height = win32api.GetSystemMetrics(1)
+midWidth = int((width + 1) / 2)
+midHeight = int((height + 1) / 2)
 
 def main():
     print("TriggerBot started.")
     pm = pymem.Pymem("cs2.exe")
     client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
 
+    state_left = win32api.GetKeyState(0x05)  # Left button up = 0 or 1. Button down = -127 or -128
+
+    toggled = False
+
     while True:
         try:
-            if not GetWindowText(GetForegroundWindow()) == "Counter-Strike 2":
-                continue
-
-            if keyboard.is_pressed(triggerKey):
+            a = win32api.GetKeyState(0x05)
+            if a != state_left:  # Button state changed
+                state_left = a
+                # print(a)
+                if a < 0:toggled=True
+                else:toggled=False
+            # if keyboard.is_pressed("x"):
+            if toggled:
                 player = pm.read_longlong(client + dwLocalPlayerPawn)
                 entityId = pm.read_int(player + m_iIDEntIndex)
 
@@ -42,11 +52,13 @@ def main():
 
                     if entityTeam != playerTeam and entityHp > 0:
                         time.sleep(uniform(0.01, 0.05))
-                        mouse.click(Button.left)
+                        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+                        time.sleep(0.1)
+                        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
-                time.sleep(0.03)
-            else:
-                time.sleep(0.1)
+                    time.sleep(0.03)
+            time.sleep(0.002)
+
         except KeyboardInterrupt:
             break
         except:
